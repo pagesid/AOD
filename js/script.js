55e317e0c99d1739d95fc9229b402bd6f929e836
unknown <ivanp314@mail.ru>
2022-12-21 21:38:21 +0300

    all

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+let ready = (callback) => {
+	if (document.readyState != "loading") callback();
+	else document.addEventListener("DOMContentLoaded", callback);
+}
+
+
+ready(() => {
+
+	//MENU
+		let iconMenu=document.querySelector(".icon-menu");
+		let body=document.querySelector("body");
+		let menuBody=document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", (e) => {
+		iconMenu.classList.toggle("active");
+		body.classList.toggle("lock");
+		menuBody.classList.toggle("active");
+	});
+
+	//SLIDER
+		let slider=document.querySelectorAll(".slider");
+	slider.forEach(el => {
+		let sliderItem = new Glide(el, {
+			type: 'carousel',
+			perView: 1,
+		});
+		sliderItem.mount();
+	});
+
+	//IBG
+	document.querySelectorAll(".ibg").forEach(el => {
+		if(el.querySelector('img')){
+			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
+			el.querySelector('img').style.display = 'none';
+		}
+	});
+});
